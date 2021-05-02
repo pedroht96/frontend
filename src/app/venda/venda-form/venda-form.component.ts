f58505bb68e5a033e22a88777ca2ce3dcd80e011
Heberth Silva <heberth.silva@live.com>
2021-05-02 17:32:14 -0300

    integração vendas com outros modulos

--- src/app/venda/venda-form/venda-form.component.ts
+++ src/app/venda/venda-form/venda-form.component.ts
@@ -1,9 +1,15 @@
-import { Venda } from './../shared/venda';
+
+
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { Venda } from './../shared/venda';
 import { VendaService } from '../shared/venda.service';
 import { Cliente } from 'src/app/cliente/shared/cliente';
 import { ClienteService } from 'src/app/cliente/shared/cliente.service';
+import { Produto } from 'src/app/produto/shared/produto';
+import { ProdutoService} from 'src/app/produto/shared/produto.service';
+import { Funcionario } from 'src/app/funcionarios/shared/funcionario';
+import { FuncionarioService } from 'src/app/funcionarios/shared/funcionario.service';
 @Component({
   selector: 'app-venda-form',
   templateUrl: './venda-form.component.html',
@@ -14,13 +20,17 @@ export class VendaFormComponent implements OnInit {
   venda: Venda = new Venda();
   title = 'Novo Venda';
   cliente!: Cliente[];
+  funcionario!: Funcionario[];
+  produto!: Produto[];
 
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private router: Router,
     private clienteService: ClienteService,
-    private vendaService: VendaService
+    private vendaService: VendaService,
+    private produtoService: ProdutoService,
+    private funcionarioService: FuncionarioService
   ) { }
 
   ngOnInit(): void {
@@ -34,10 +44,17 @@ export class VendaFormComponent implements OnInit {
 
     }
 
-    this.clienteService.getAll().subscribe(cliente =>{
+    this.clienteService.getAll().subscribe(cliente => {
       this.cliente = cliente;
     });
 
+    this.produtoService.getAll().subscribe(produto => {
+      this.produto = produto;
+    });
+
+    this.funcionarioService.getAll().subscribe(funcionario => {
+      this.funcionario = funcionario;
+    });
 
 
 
